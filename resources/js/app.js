b566878b398e01d893a355d3c8bf092234c70492
Nazmus Sakib <sukku.mia@gmail.com>
2019-10-29 23:18:33 +0600

    Initialize vue and js libraries

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 require('./bootstrap');
+
+
+const app = new Vue({
+    el: '#app'
+});
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -7,8 +7,15 @@ window._ = require('lodash');
  */
 
 window.axios = require('axios');
+window.Vue = require('vue');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+}
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
